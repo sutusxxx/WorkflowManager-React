1cee4a98b5a7d388bc650f3ee151e0764e005882
sutusxxx <sutus.laszlo96@gmail.com>
2026-04-05 13:16:37 +0200

    Format code.

--- app/features/issues/IssueList.tsx
+++ app/features/issues/IssueList.tsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import SortableList from "~/components/lists/SortableList";
 
-
-const list = ["Item1", "Item2", "Item3", "Item4"];
-
 export default function IssueList() {
     const [items, setItems] = useState([1, 2, 3, 4, 5]);
 
